d104b058ea6aa0ba0a5839ba7d67b3dbfe990af2
shubham harkare <shubham.21910407@viit.ac.in>
2020-06-23 18:07:31 +0530

    'first'

---
+++ learning_templates/basic_app/templatetags/my_extras.py
@@ -0,0 +1,13 @@
+from django import template
+
+register=template.Library()
+
+
+def cut(value,arg):
+    """
+    this cuts out all value of 'arg'
+    """
+    return value.replace(arg,'')
+
+
+register.filter('cut',cut)
